1534852b618da88fb896f47c44d0f5d0722cc6ad
atlowell <atlowell42@gmail.com>
2016-12-05 00:49:33 -0500

    Worked on Registration page/redirecting.  Still needs the algorithm/database connection.

--- src/main/java/net/mybluemix/gateway/listener/AppListener.java
+++ src/main/java/net/mybluemix/gateway/listener/AppListener.java
@@ -3,6 +3,7 @@ package net.mybluemix.gateway.listener;
 import com.mongodb.MongoClient;
 import com.mongodb.MongoClientOptions;
 import com.mongodb.MongoClientURI;
+import net.mybluemix.gateway.authenticator.LoginAuthProvider;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
@@ -30,6 +31,9 @@ public class AppListener implements ServletContextListener {
 		MongoClientURI uri = new MongoClientURI("mongodb://admin:XFEXEQSJSHMCSVSE@sl-us-dal-9-portal.3.dblayer.com:17281,sl-us-dal-9-portal.0.dblayer.com:17281/admin?ssl=true");
 		MongoClient client = new MongoClient(uri);
 		sc.setAttribute("datasource", client);
+
+		LoginAuthProvider lap = new LoginAuthProvider();
+		lap.setServletContext(sc);
 	}
 
 	@Override
